d8cd39b254665fcf17cf56a32fd319117bdf989e
Vladislav Manakov <footzi@mail.ru>
2021-12-09 21:38:38 +0300

    Убирает поля пароля при ответе на клиент

--- src/modules/signIn/SignIn.model.ts
+++ src/modules/signIn/SignIn.model.ts
@@ -7,9 +7,13 @@ import { E } from '../../constans';
 export default class SignInModel {
   public static async getUser(login: string): Promise<IUser | Error | undefined> {
     try {
-      const response = await getRepository(User).findOne({ login });
+      const user = await getRepository(User).findOne({ login });
 
-      return response;
+      if (user) {
+        delete user.password;
+      }
+
+      return user;
     } catch (err) {
       throw errorTypeMessage(E.critical, err);
     }
